d8a0f682ae822a86cf637cb54e62b916427e0491
tim <tim@timbargo.com>
2022-02-10 12:17:35 -0600

    Outline pan/tilt subsystem

--- software/subsystems/pt-ctrl.py
+++ software/subsystems/pt-ctrl.py
@@ -1,1 +1,34 @@
 print('Pan/Tilt Control Spawned')
+
+import RPi.GPIO as GPIO
+import time
+
+class ptCtrl:
+
+    # Initialize the servos and pins
+    def initialize():
+    
+    # Overwrite the home position with the current position
+    def setHome(x,y):
+
+    # Traverse to the home position
+    def goHome():
+
+    # Traverse to the desired coordinates at the desired speed
+    # (Tim) We can consider exlcuding this function if deemed unnecessary.
+    def setPos(x,y,vel):
+
+    def panLeft(distance):
+
+    def panRight(distance):
+
+    def tiltUp(distance):
+
+    def tiltDown(distance):
+
+def main():
+    initialize()
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
