74cf27279016948f0bf042644c48bb58dc0309d4
timbargo <tim@timbargo.com>
2022-02-11 13:52:28 -0600

    Edit servo control with Zhongyi changes

--- software/subsystems/pt-ctrl.py
+++ software/subsystems/pt-ctrl.py
@@ -1,22 +1,24 @@
-print('Pan/Tilt Control Spawned')
-
 import RPi.GPIO as GPIO
 import time
-
+     
 class ptCtrl:
+
+    def __init__(self, config):
 
     # Initialize the servos and pins
     def initialize():
     
     # Overwrite the home position with the current position
-    def setHome(x,y):
+    def setHome(x, y):
 
     # Traverse to the home position
     def goHome():
 
     # Traverse to the desired coordinates at the desired speed
     # (Tim) We can consider exlcuding this function if deemed unnecessary.
-    def setPos(x,y,vel):
+    def setPos(x, y, vel):
+
+    def offsetPos(x, y, vel):
 
     def panLeft(distance):
 
@@ -27,6 +29,7 @@ class ptCtrl:
     def tiltDown(distance):
 
 def main():
+    print('Pan/Tilt Control Spawned')
     initialize()
 
 if __name__ == "__main__":
